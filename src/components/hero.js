9add5fa24ec7305da035a12f960d8ade3baaaff7
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2020-02-10 01:58:25 +0700

    the new colors

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,26 +7,22 @@ const ImageBackground = styled(BackgroundImage)`
   background-position: top 50% center;
   background-size: cover;
   height: 50vh;
-  /* override the default margin for sibling elements  */
   + * {
     margin-top: 0;
   }
 `;
 
 const TextBox = styled('div')`
-  background-image: linear-gradient(to top, #ddbbffdd 2rem, #ddbbff00);
+  background-image: linear-gradient(to top, #0000ffdd 2rem, #ddbbff00);
   display: flex;
   flex-direction: column;
   height: 100%;
   justify-content: flex-end;
-  padding: 0 5vw 2rem;
+  padding: 0 calc((100vw - 550px) / 2) 2rem;
   width: 100%;
-  @media (min-width: calc(550px + 10vw)) {
-    padding-left: calc((100vw - 550px) / 2);
-    padding-right: calc((100vw - 550px) / 2);
   }
   h1 {
-    text-shadow: 1px 1px 3px #eeddff66;
+    text-shadow: 1px 1px 3px #0000ff66;
     font-size: 2.25rem;
   }
   p,
